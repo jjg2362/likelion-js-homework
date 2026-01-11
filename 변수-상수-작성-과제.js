3f78eff9f42469ad942c6123dcf92449c5923d3f
JKCho <wornjs2362@gmail.com>
2026-01-11 19:34:09 +0900

    변수 상수 작성 과제 완료

---
+++ 변수-상수-작성-과제.js
@@ -0,0 +1,42 @@
+
+// 1. 영화 제목을 저장할 변수를 선언하고 좋아하는 영화 제목을 할당하세요.
+let favoriteMovieName;
+favoriteMovieName = '인셉션'
+
+// 2. "빛의 속도(299,792,458 m/s)"를 담을 상수를 선언하세요.
+const lightSpeed = 299792458;
+
+// 3. "이메일 인증 완료 여부"를 확인하는 불리언 타입 변수를 선언하고 적절한 값을 할당하세요.
+let isEmailVerified = false;
+ isEmailVerified = true;
+
+// 4. "상품 재고 수량(예: 47개)"을 담는 변수를 선언하고 숫자 값을 할당하세요.
+let productQuantity = 47;
+
+// 5. "회원 포인트(예: 15,800점)"를 담는 변수를 작성하세요.
+let userPoint = 15800;
+
+// 6. API 서버의 "기본 URL(예: https://api.example.com)을 담는 상수를 작성하고 값을 할당하세요.
+const baseUrl = 'https://api.example.com';
+
+// 7. 게시글의 "조회수(예: 1,234)"를 담는 변수를 선언하고 값을 할당하세요.
+let view = 1234;
+
+// 8. "배송 상태('준비중', '배송중', '배송완료' 중 하나)"를 담는 변수를 작성해보세요.
+const DELIVERY_READY = 'READY';
+const DELIVERY_SHIPPING = 'SHIPPING';
+const DELIVERY_FINISH = 'FINISH';
+
+let deliveryState = DELIVERY_READY;
+
+// 9. "쿠폰 사용 가능 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
+let isCouponAvaiable = true;
+
+// 10. "최대 업로드 파일 크기(예: 10MB를 바이트 단위로 10485760)"를 담는 상수를 작성하세요.
+const MAX_UPLOAD_FILE_SIZE = 10485760;
+
+// 11. "사용자 등급 점수(예: 85.5점)"를 담는 변수를 선언하고 소수점이 포함된 값을 할당하세요.
+let userScore = 85.5;
+
+// 12. "알림 수신 동의 여부"를 담는 변수를 선언하고 불리언 타입 값을 설정하세요.
+let isNotificationAllowed = false;
